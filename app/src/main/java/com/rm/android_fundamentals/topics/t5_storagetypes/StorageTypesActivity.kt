0fe7e20056e948200c87b859b56060bd20846e7e
rkmall <rupeshmallnav@gmail.com>
2024-03-08 00:15:55 +0000

    Added methods to read and write for external storage and shared preferences

--- app/src/main/java/com/rm/android_fundamentals/topics/t5_storagetypes/StorageTypesActivity.kt
+++ app/src/main/java/com/rm/android_fundamentals/topics/t5_storagetypes/StorageTypesActivity.kt
@@ -1,11 +1,15 @@
 package com.rm.android_fundamentals.topics.t5_storagetypes
 
 import android.Manifest
+import android.content.Context
 import android.os.Bundle
+import android.os.Environment
+import android.text.method.ScrollingMovementMethod
 import androidx.core.app.ActivityCompat
 import com.rm.android_fundamentals.base.BaseActivity
 import com.rm.android_fundamentals.databinding.ActivityStorageTypesBinding
 import com.rm.android_fundamentals.utils.hasPermission
+import com.rm.android_fundamentals.utils.toastMessage
 import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.File
@@ -23,6 +27,10 @@ class StorageTypesActivity : BaseActivity() {
 
         checkPermissions()
 
+        if (isExternalStorageWritable() && isExternalStorageReadable()) {
+            toastMessage(this, "External storage dirs available for read and write")
+        }
+
         selectStorageType()
     }
 
@@ -30,55 +38,125 @@ class StorageTypesActivity : BaseActivity() {
         binding.apply {
             btnCreateFile.setOnClickListener {
                 when (rg.checkedRadioButtonId) {
-                    rbInternal.id -> internalStorageWriteFile()
-                    rbInternalCache.id -> internalStorageWriteCacheFile()
-                    else -> {}
+                    rbInternal.id -> writeFile(filesDir, SUB_DIR)
+                    rbInternalCache.id -> writeFile(cacheDir, null)
+                    rbExternal.id -> writeFile(getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)!!, SUB_DIR)
+                    rbExternalCache.id -> writeFile(externalCacheDir!!, null)
+                    rbExternalStorage.id -> writeFile(Environment.getExternalStorageDirectory(), Environment.DIRECTORY_DOCUMENTS)
+                    rbExternalStoragePublic.id -> writeFile(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), SUB_DIR)
+                    rbSharedPreferences.id -> writeToSharedPreferences()
                 }
             }
 
             btnReadFile.setOnClickListener {
                 when (rg.checkedRadioButtonId) {
-                    rbInternal.id -> internalStorageReadFile()
-                    rbInternalCache.id -> internalStorageReadCacheFile()
-                    else -> {}
+                    rbInternal.id -> readFile(filesDir, SUB_DIR)
+                    rbInternalCache.id -> readFile(cacheDir, null)
+                    rbExternal.id -> readFile(getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)!!, SUB_DIR)
+                    rbExternalCache.id -> readFile(externalCacheDir!!, null)
+                    rbExternalStorage.id -> readFile(Environment.getExternalStorageDirectory(), Environment.DIRECTORY_DOCUMENTS)
+                    rbExternalStoragePublic.id -> readFile(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), SUB_DIR)
+                    rbSharedPreferences.id -> readFromSharedPreferences()
+                }
+            }
+
+            btnListFiles.setOnClickListener {
+                when (rg.checkedRadioButtonId) {
+                    rbInternal.id -> listFiles(filesDir, SUB_DIR)
+                    rbInternalCache.id -> listFiles(cacheDir, null)
+                    rbExternal.id -> listFiles(getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)!!, SUB_DIR)
+                    rbExternalCache.id -> listFiles(externalCacheDir!!, null)
+                    rbExternalStorage.id -> listFiles(Environment.getExternalStorageDirectory(), Environment.DIRECTORY_DOCUMENTS)
+                    rbExternalStoragePublic.id -> listFiles(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), SUB_DIR)
                 }
             }
         }
     }
 
-    private fun internalStorageWriteFile() {
-        val dir = File(filesDir, INTERNAL_STORAGE_TEST_DIR)
-        if (!dir.exists()) {
-            dir.mkdir()
+    private fun writeFile(parent: File, child: String?) {
+        binding.apply {
+            val fileName = txtInputFileName.text.toString()
+            val fileContent = txtInputFileContent.text.toString()
+            val dir: File?
+            val file: File?
+            if (fileName.isNotBlank() && fileContent.isNotBlank()) {
+                if (child != null) {
+                    dir = File(parent, child)
+                    if (!dir.exists()) {
+                        dir.mkdir()
+                    }
+                    file = File(dir, fileName)
+                } else {
+                    dir = parent
+                    file = File.createTempFile(fileName, null, dir)
+                }
+
+                if (file != null) {
+                    BufferedWriter(FileWriter(file)).use {
+                        it.append(fileContent).flush()
+                    }
+                    binding.txtFileInfo.text = file.absolutePath
+                }
+            } else {
+                toastMessage(this@StorageTypesActivity, "Please enter both file name and file content")
+            }
+        }
+    }
+
+    private fun listFiles(parent: File, child: String?) {
+        val dir = if (child != null) File(parent, child) else parent
+        val result = StringBuilder()
+        result.append("Dir: $dir\n")
+        if (dir.exists()) {
+            dir.listFiles()?.forEachIndexed { index, value ->
+                result.append("${index + 1}. ")
+                result.append(value.absolutePath.substringAfterLast("/"))
+                result.append('\n')
+            }
+        } else {
+            result.append("Directory doesn't exist")
         }
 
-        val file = File(dir, "testFile1.txt")
-        val fileContent = "Some content for testing, line 1.\nTesting line 2.\nTesting line 3."
-        binding.tvFileName.text = file.absolutePath
-
-        writeFileContent(file, fileContent)
+        binding.txtFileInfo.text = result
+        binding.txtFileInfo.movementMethod = ScrollingMovementMethod()
     }
 
-    private fun internalStorageReadFile() {
-        val dir = File(filesDir, INTERNAL_STORAGE_TEST_DIR)
-        val file = File(dir, "testFile1.txt")
-        val result = readFileContent(file)
-        binding.tvFileContent.text = result
+    private fun readFile(parent: File, child: String?) {
+        val dir = if (child != null) File(parent, child) else parent
+        val fileName = binding.txtInputFileName.text.toString()
+        if (fileName.isNotBlank()) {
+            val result = StringBuilder()
+            val file = File(dir, fileName)
+            result.append("File: ${file.absolutePath}\n")
+            if (file.exists()) {
+                BufferedReader(FileReader(file)).use {
+                    while (true) {
+                        val line = it.readLine() ?: break
+                        result.append(line)
+                        result.append('\n')
+                    }
+                }
+            } else {
+                result.append("File doesn't exist")
+            }
+            binding.txtFileInfo.text = result
+        } else {
+            toastMessage(this, "Please enter file name")
+        }
     }
 
-    private lateinit var tempFileName: String  // temp files are appended with random numbers
-    private fun internalStorageWriteCacheFile() {
-        val file = File.createTempFile("testCacheFile1", ".txt", cacheDir)
-        binding.tvFileName.text = file.absolutePath
-        tempFileName = file.absolutePath.substringAfterLast('/')
-        val fileContent = "Content for cache file"
-        writeFileContent(file, fileContent)
+    private fun writeToSharedPreferences() {
+        val sharedPreferences = getPreferences(Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putInt("saved_int", 10)
+        editor.apply()
+        binding.txtFileInfo.text = 10.toString()
     }
 
-    private fun internalStorageReadCacheFile() {
-        val file = File(cacheDir, tempFileName)
-        val result = readFileContent(file)
-        binding.tvFileContent.text = result
+    private fun readFromSharedPreferences() {
+        val sharedPreferences = getPreferences(Context.MODE_PRIVATE)
+        val savedInt = sharedPreferences.getInt("saved_int", 0)
+        binding.txtFileInfo.text = savedInt.toString()
     }
 
     private fun checkPermissions() {
@@ -92,30 +170,78 @@ class StorageTypesActivity : BaseActivity() {
         }
     }
 
-    private fun readFileContent(file: File): String {
-        val result = StringBuilder()
-        if (file.exists()) {
-            BufferedReader(FileReader(file)).use {
-                while (true) {
-                    val line = it.readLine() ?: break
-                    result.append(line)
-                    result.append('\n')
-                }
-            }
-        }
-        return result.toString()
+    private fun isExternalStorageWritable(): Boolean {
+        val state = Environment.getExternalStorageState()
+        return Environment.MEDIA_MOUNTED == state
     }
 
-    private fun writeFileContent(file: File, fileContent: String) {
-        BufferedWriter(FileWriter(file)).use {
-            it.append(fileContent).flush()
-        }
+    private fun isExternalStorageReadable(): Boolean {
+        val state = Environment.getExternalStorageState()
+        return Environment.MEDIA_MOUNTED == state || Environment.MEDIA_MOUNTED_READ_ONLY == state
     }
 
     override fun getTitleToolbar(): String = "Storage types activity"
 
     companion object {
         private const val READ_WRITE_EXTERNAL_STORAGE = 1001
-        private const val INTERNAL_STORAGE_TEST_DIR = "test_dir"
+        private const val SUB_DIR = "test_dir"
     }
+
+    /*Stub methods:
+    private fun writeToInternalStorage() {
+        val dir = File(filesDir, "test_dir") // access internal storage with "filesDir"
+        if(!dir.exists()) { dir.mkdir() } // create sub-dir to organize file
+        val file = File(dir, "test_file888.txt") // create file under sub-dir
+        BufferedWriter(FileWriter(file)).use {// write to the file
+            it.append("Some content for the test file.\nLine2\nLine3")
+        }
+    }
+
+    private fun writeToInternalCacheStorage() {
+        // Access internal cache storage with "cacheDir"
+        val file = File.createTempFile("test_cache_file", ".txt", cacheDir) // create file
+        BufferedWriter(FileWriter(file)).use {  // write to the file
+            it.append("Some content for the test file.\nLine2.\nLine3")
+        }
+    }
+
+    private fun writeToExternalStorage() {
+        val dir = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS) // get external storage
+        val subDir = File(dir, "MyDocs") // create sub-dir to organize files
+        if (!subDir.exists()) { subDir.mkdir() }
+        val file = File(subDir, "test1.txt") // create file under sub-dir
+
+        BufferedWriter(FileWriter(file)).use {// write to the file
+            it.append("Some content for test file")
+        }
+    }
+
+    private fun writeToExternalCacheStorage() {
+        // Access external cache storage with "externalCacheDir"
+        val file = File.createTempFile("test_file", ".txt", externalCacheDir) // create file
+        BufferedWriter(FileWriter(file)).use { // write to the file
+            it.append("Some content for test file")
+        }
+    }
+
+    private fun writeToExternalStorageDir() {
+        val dir = Environment.getExternalStorageDirectory()
+        val subDir = File(dir, Environment.DIRECTORY_DOCUMENTS) // create sub-dir to organize files
+        val file = File(subDir, "test1.txt") // create file under sub-dir
+
+        BufferedWriter(FileWriter(file)).use {// write to the file
+            it.append("Some content for test file")
+        }
+    }
+
+    private fun writeToExternalStoragePublicDit() {
+        val dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS)
+        val subDir = File(dir, "MyDocs") // create sub-dir to organize files
+        if (!subDir.exists()) { subDir.mkdir() }
+        val file = File(subDir, "test1.txt") // create file under sub-dir
+
+        BufferedWriter(FileWriter(file)).use {// write to the file
+            it.append("Some content for test file")
+        }
+    }*/
 }
