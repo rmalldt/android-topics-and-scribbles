56ce31cd973806b380e47ae005251b0ce4c43bcf
rkmall <rupeshmallnav@gmail.com>
2024-05-01 15:31:16 +0100

    Added README

---
+++ README.md
@@ -0,0 +1,2 @@
+## About
+**android-topics-and-scribbles** is an attempt to create a collection of code examples on various fundamental Android topics. It currently uses View framework for UI.
--- app/src/main/java/com/rm/android_fundamentals/MainActivity.kt
+++ app/src/main/java/com/rm/android_fundamentals/MainActivity.kt
@@ -19,7 +19,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     private lateinit var navController: NavController
-
     private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
